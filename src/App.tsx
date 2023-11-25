c0a26a7060e3ec2d03b2130c80835a1e06c474f9
Dmitry Andreyev <Krygliy.ya@gmail.com>
2023-11-25 20:52:06 +0700

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,35 @@ import Friends from './components/Navbar/Pages/Friends/Friends.tsx';
 import News from './components/Navbar/Pages/News/News.tsx';
 import MyProfile from './components/Navbar/Pages/MyProfile/MyProfile.tsx';
 import FriendProfile from './components/Navbar/Pages/Friends/FriendProfile/FriendProfile.tsx';
-
+import Popup from './components/Popup/Popup.tsx';
+import { Context } from './Context.tsx';
 
 const App: React.FC = () => {
+   const [activePopup, setActivePopup] = React.useState(false);
+   const popUpLoginMenu = () => {
+      setActivePopup(!activePopup);
+   };
    return (
-      <div className="app-wrapper">
-         <Header />
-         <Navbar />
-       
-         <div className="app-wrapper-content">
-            <Routes>
-               <Route path="/news" element={<News />} />
-               <Route path="/friends" element={<Friends />} />
-               <Route path="/myprofile" element={<MyProfile />} />
-			<Route path = '/profilePage' element={<FriendProfile/>} />
-            </Routes>
-            
+      <Context.Provider value={{ popUpLoginMenu, activePopup, setActivePopup }}>
+         <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <div className="app-container__popup">
+               <div className="app-wrapper__popup ">
+                  <Popup />
+               </div>
+            </div>
+
+            <div className="app-wrapper-content">
+               <Routes>
+                  <Route path="/news" element={<News />} />
+                  <Route path="/friends" element={<Friends />} />
+                  <Route path="/myprofile" element={<MyProfile />} />
+                  <Route path="/profilePage" element={<FriendProfile />} />
+               </Routes>
+            </div>
          </div>
-      </div>
+      </Context.Provider>
    );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import LogIn from './LogInMenu/LoginMenu.tsx';
+
 
 const Header = () => {
+
    return (
       <div className={styles.header__container}>
          <div className={styles.header__wrapper}>
             <div>SOCIAL</div>
+            <LogIn />
          </div>
       </div>
    );
