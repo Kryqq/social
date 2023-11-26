422c5d4fc9ccee730d8e38d2206589d8be68cb1f
Dmitry Andreyev <Krygliy.ya@gmail.com>
2023-11-26 22:01:31 +0700

    textArea

--- src/components/Navbar/Pages/MyProfile/MyProfile.tsx
+++ src/components/Navbar/Pages/MyProfile/MyProfile.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import styles from './MyProfile.module.scss';
 
 const MyProfile = () => {
+   const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+   const [post, setPost] = React.useState('');
+
+   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setPost(e.target.value);
+      console.log(post);
+   };
+
    return (
       <div className={styles.myProfile__container}>
          <div className={styles.myProfile__upperContent}>
@@ -18,8 +26,8 @@ const MyProfile = () => {
             <div className={styles.myProfile__functions}></div>
             <div className={styles.posts__wrapper}>
                <div className={styles.posts__area}>
-                  <textarea className={styles.postArea}></textarea>
-                  <button className={styles.postBtn}></button>
+                  <textarea onChange={onChange} ref={textAreaRef} value={post} className={styles.postArea}></textarea>
+                  <button className={styles.postBtn}>add post</button>
                </div>
                <div className={styles.posts__added}>posts</div>
             </div>
