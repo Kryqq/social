086c42388c31ea60ae0840735284dc7507ef2ff2
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-01-10 23:04:18 +0700

    asdac

--- src/components/Navbar/Pages/Friends/Friend/PossibleFriend.tsx
+++ src/components/Navbar/Pages/Friends/Friend/PossibleFriend.tsx
@@ -4,7 +4,21 @@ import { useAppDispatch } from '../../../../../redux/store/store.tsx';
 import { deleteFriendSlice } from '../../../../../redux/slices/FriendSlice/FrinendsSlice.ts';
 import { Link } from 'react-router-dom';
 import { addFriend } from '../../../../../redux/slices/MyProfileSlice/MyProfileSlice.ts';
-
+import { Button } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+const theme = createTheme({
+   palette: {
+      primary: {
+         main: blue[500],
+      },
+      secondary: {
+         main: '#003c6c',
+      },
+   },
+});
 export const Friend: React.FC = ({ id, name, gender, image, location, species }) => {
    const dispatch = useAppDispatch();
    const deleteFriend = () => {
@@ -24,44 +38,37 @@ export const Friend: React.FC = ({ id, name, gender, image, location, species })
                   <img src={image}></img>
                </Link>
                <div className={styles.button__wrapper}>
-                  <button onClick={deleteFriend} className={styles.button__addTo_friend}>
-                     <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                     >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4c.348 0 .686 .045 1.009 .128" />
-                        <path d="M16 19h6" />
-                     </svg>
+                  <Button
+                     sx={{
+                        width: 180,
+                        backgroundColor: theme.palette.primary.main,
+                        ':hover': {
+                           backgroundColor: theme.palette.secondary.main,
+                        },
+                     }}
+                     variant="contained"
+                     onClick={deleteFriend}
+                     startIcon={<PersonRemoveIcon sx={{ width: 25, height: 25 }} />}
+                     className={styles.button__addTo_friend}
+                  >
                      Больше не показывать
-                  </button>
-                  <button onClick={handleAddFriend} className={styles.button__send_message}>
-                     <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                     >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                        <path d="M16 19h6" />
-                        <path d="M19 16v6" />
-                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4" />
-                     </svg>
+                  </Button>
+                  <Button
+                     sx={{
+                        margin: 1,
+                        width: 180,
+                        backgroundColor: theme.palette.primary.main,
+                        ':hover': {
+                           backgroundColor: theme.palette.secondary.main,
+                        },
+                     }}
+                     onClick={handleAddFriend}
+                     className={styles.button__send_message}
+                     variant="contained"
+                     startIcon={<PersonAddIcon sx={{ width: 25, height: 25 }} />}
+                  >
                      Добавить в друзья
-                  </button>
+                  </Button>
                </div>
             </div>
             <div className={styles.span__wrapper}>
